d8ae2a0c40ea31ae5c4d5ef2f5aa8ab5537b6d47
Andreas Mohr <andim2@users.sf.net>
2015-07-28 17:15:26 +0200

    Feature: caching: implement limiting of WebCache instance data size amount.

--- SvnBridgeLibrary/Cache/WebCache.cs
+++ SvnBridgeLibrary/Cache/WebCache.cs
@@ -12,6 +12,23 @@ namespace SvnBridge.Cache
         // http://stackoverflow.com/questions/8092448/should-i-use-httpruntime-cache
         private readonly System.Web.Caching.Cache cache = HttpRuntime.Cache;
         private readonly TimeSpan slidingExpiration = TimeSpan.FromHours(2);
+        // A suitable value might be a moderately large value of 100MB -
+        // reason being that nowadays this is one fifth
+        // of the resources of a small system,
+        // thus we should not exceed that.
+        private readonly int maxEntriesLimit = MemoryUseToMaxEntriesLimit(
+            100
+        );
+
+        private static int MemoryUseToMaxEntriesLimit(int sizeInMB)
+        {
+            // Given an assumed average size of 1kB per entry
+            // we will end up at this limit value
+            // for medium-severe cache resource use.
+            const int entriesPerMB = 1000;
+            int maxEntriesLimit = sizeInMB * entriesPerMB;
+            return maxEntriesLimit;
+        }
 
         public virtual CachedResult Get(string key)
         {
@@ -20,6 +37,8 @@ namespace SvnBridge.Cache
 
         public virtual void Set(string key, object obj)
         {
+            HandleLimit(); // see useful docs at method
+
             cache.Add(GetKeyCooked(key),
                 new CachedResult(obj), 
                 null, 
@@ -27,6 +46,32 @@ namespace SvnBridge.Cache
                 slidingExpiration,
                 CacheItemPriority.Default, 
                 null);
+        }
+
+        /// <remarks>
+        /// While cache class has some builtin limiting mechanisms,
+        /// MSDN says:
+        /// "EffectivePercentagePhysicalMemoryLimit is introduced in the .NET Framework version 3.5",
+        /// thus it seems older versions may have insufficient handling
+        /// and thus I decided to go the version-compatible route,
+        /// by implementing manual limiting of the cache object.
+        ///
+        /// IMPORTANT: definitely do this "potentially harmful" Limit handling
+        /// only *prior* to Add()ing the new key!
+        /// And in general
+        /// decide to do "annoying" limit check extra handling
+        /// in a *non-hotpath* (yet *reliably-invoked*!) area.
+        /// E.g. it should be done
+        /// in Set()-side (assumed-non-hotpath) handling
+        /// rather than assumed-busier Get()...
+        /// </remarks>
+        private void HandleLimit()
+        {
+            bool isBelowEqualLimit = (maxEntriesLimit >= cache.Count);
+            if (!(isBelowEqualLimit))
+            {
+                Clear();
+            }
         }
 
         public virtual void Clear()
