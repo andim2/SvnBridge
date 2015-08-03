12d408c6cb2a44e225a5994a76dc0ac1465ba251
Andreas Mohr <andim2@users.sf.net>
2015-08-03 13:31:46 +0200

    Correction: symmetry: fix off-by-1 bug (caused needless reassign) in one of the two cases of path stripping.

--- SvnBridgeLibrary/SourceControl/TFSSourceControlProvider.cs
+++ SvnBridgeLibrary/SourceControl/TFSSourceControlProvider.cs
@@ -2224,7 +2224,7 @@ namespace SvnBridge.SourceControl
             }
 
             item.Id = sourceItem.ItemId;
-            if (sourceItem.RemoteName.Length >= rootPath.Length)
+            if (sourceItem.RemoteName.Length > rootPath.Length)
             {
                 item.Name = sourceItem.RemoteName.Substring(rootPath.Length);
             }
