9016fde17a435749eedbb10dc94e31f4bff11be4
Andreas Mohr <andim2@users.sf.net>
2015-10-22 15:51:40 +0200

    Feature: WebDAV: add (partial) support for DAV properties getcontentlanguage / getcontenttype / source / supportedlock.
    These have been queried by Konqueror 4.8.5.

--- SvnBridgeLibrary/RequestHandlers/PropFindHandler/FileNode.cs
+++ SvnBridgeLibrary/RequestHandlers/PropFindHandler/FileNode.cs
@@ -1,3 +1,4 @@
+using System; // String.Format()
 using CodePlex.TfsLibrary.RepositoryWebSvc; // ItemType
 using SvnBridge.Handlers; // RequestHandlerBase
 using SvnBridge.SourceControl; // ItemMetaData, TFSSourceControlProvider
@@ -68,20 +69,49 @@ namespace SvnBridge.Nodes
                     return GetExecutable();
                 case "version-name":
                     return GetVersionName();
+                case "getcontentlanguage": // e.g. Konqueror 4.8.5 webdav://
+                    return GetContentLanguage();
                 case "getcontentlength":
                     return GetContentLength();
+                case "getcontenttype": // e.g. Konqueror 4.8.5 webdav://
+                    return GetContentType();
                 case "lockdiscovery":
                     return GetLockDiscovery();
                 case "md5-checksum":
                     return GetMd5Checksum();
                 case "quota-available-bytes": // Queried by e.g. davfs2.
                     return GetQuotaAvailableBytes();
+                case "source": // e.g. Konqueror 4.8.5 webdav://
+                    return GetSource(handler);
+                case "supportedlock": // e.g. Konqueror 4.8.5 webdav://
+                    return GetSupportedLock();
                 default:
                     return null;
             }
         }
 
         #endregion
+
+        /// <remarks>
+        /// RFC2068
+        ///   "14.13 Content-Language":
+        /// "
+        /// If no Content-Language is specified, the default is that the content
+        /// is intended for all language audiences. This may mean that the sender
+        /// does not consider it to be specific to any natural language, or that
+        /// the sender does not know for which language it is intended.
+        /// "
+        /// I believe that items in TFS do carry some encoding field somewhere
+        /// (was that in history queries?)
+        /// which could be used to clumsily infer a "language" from.
+        /// However, as long as we don't have that available
+        /// simply return "unknown" language.
+        /// </remarks>
+        private string GetContentLanguage()
+        {
+            string contentLanguageUnknown = "";
+            return contentLanguageUnknown;
+        }
 
         private string GetContentLength()
         {
@@ -96,6 +126,48 @@ namespace SvnBridge.Nodes
             // as long as we are only making use of it at this single place here.
             int content_length = sourceControlProvider.ReadFile(item).Length;
             return "<lp1:getcontentlength>" + content_length + "</lp1:getcontentlength>";
+        }
+
+        /// <remarks>
+        /// TODO: Hmm, what to do here?
+        /// TFS possibly does not directly offer functionality
+        /// to indicate content type,
+        /// so perhaps we would have to resort
+        /// to manually and painfully passing our huge-blob content
+        /// to some system-side content type (MIME type!!) detection API.
+        /// So, the best we can do for now
+        /// is indicate unknown MIME type (empty string),
+        /// since subsequent layers likely are currently better prepared
+        /// to make some guesses.
+        /// And quite possibly we shouldn't directly indicate "unknown" type
+        /// ("application/octet-stream") either,
+        /// since that would possibly forego any type guessing
+        /// that might happen subsequently.
+        /// So, really do return an empty string only.
+        /// And yes indeed, now that we do support this property
+        /// with "unsupported" hint (empty string),
+        /// Konqueror does seem to do MIME type heuristics
+        /// since it does switch its view
+        /// from bare file entries listing
+        /// to properly item type distinct icon view.
+        /// </remarks>
+        private string GetContentType()
+        {
+            string unknownContentType = "";
+            string contentType;
+
+            bool isFolder = (ItemType.Folder == item.ItemType);
+            bool haveContentType = !(isFolder);
+            if (haveContentType)
+            {
+                contentType = unknownContentType;
+            }
+            else
+            {
+                contentType = "";
+            }
+
+            return contentType;
         }
 
         private string GetDeadPropCount()
@@ -276,6 +348,71 @@ namespace SvnBridge.Nodes
         {
             // STUB (I don't think we should/can indicate any quota support in this server).
             return "<D:quota-available-bytes/>";
+        }
+
+        /// <remarks>
+        ///  RFC2518:
+        ///    "13.10.  source Property"
+        ///    "5.4 Source Resources and Output Resources"
+        /// </remarks>
+        private string GetSource(RequestHandlerBase handler)
+        {
+            //string test1 = handler.GetLocalPathFromUrl(item.Name);
+            //string test2 = handler.GetLocalPath(item.Name);
+            string href = Href(handler);
+            // In our case I'd think
+            // that the output (processed) location
+            // does not differ from the source (raw item) location,
+            // thus it ought to be identical.
+            string src = href;
+            string dst = href;
+            // Hmm, that cannot be done easily
+            // since D:source likely is expected to return
+            // fully host-based URIs,
+            // however host parts are only available in context.Request.Uri areas
+            // which are not (supposed to be) available
+            // in FileNode-restricted sub scopes
+            // (after all the host part is emphatically *NOT*
+            // a part of the "inner" properties of a file node!).
+            // One could possibly even argue
+            // that we have a layer violation
+            // even within the spec
+            // in that it suggests returning host-related full paths
+            // at the property implementation of a specific node.
+            // The only meaningful solution would probably be
+            // to specially handle "source" property
+            // at the PROPFIND layer,
+            // by passing the INode into a helper method
+            // which produces host-augmented src, dst pairs for an INode,
+            // to then be generated into XML format.
+            // Oh well, so perhaps simply return an empty <D:source> elem.
+            return "<D:source/>";
+            //return GenerateSourceProperty(src, dst);
+        }
+
+        /// <remarks>
+        /// TODO: we should be supporting resource locking
+        /// (should be doable via usual TFS checkout for edit stuff etc.).
+        /// As long as we don't support it,
+        /// do indicate so.
+        /// </remarks>
+        private static string GetSupportedLock()
+        {
+            string noLockSupported = "";
+            return noLockSupported;
+        }
+
+        private static string GenerateSourceProperty(string src, string dst)
+        {
+            return String.Format(
+                "<D:source>" +
+                "<D:link>" +
+                "<D:src>{0}</D:src>" +
+                "<D:dst>{1}</D:dst>" +
+                "</D:link>" +
+                "</D:source>",
+                src,
+                dst);
         }
     }
 }
